9b4079a316602f1194eb13aeb16a5f24b5a94a05
pualsa <pualsa@users.noreply.github.com>
2021-01-24 12:56:25 +0100

    renaming

--- ng-auction-spring/src/main/java/org/pualsa/ngauction/FirstdemoApplication.java
+++ ng-auction-spring/src/main/java/org/pualsa/ngauction/FirstdemoApplication.java
@@ -1,4 +1,4 @@
-package org.alsa.firstdemo;
+package org.pualsa.ngauction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
