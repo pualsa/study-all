77b436e238456900ebf73fc40bfdee1774dbb156
pualsa <pualsa@users.noreply.github.com>
2025-06-01 19:31:40 +0200

    Configure WebSocket buffer sizes and add handshake interceptor

--- ng-auction-spring/src/main/java/org/pualsa/ngauction/websocket/WebSocketConfiguration.java
+++ ng-auction-spring/src/main/java/org/pualsa/ngauction/websocket/WebSocketConfiguration.java
@@ -1,8 +1,7 @@
 package org.pualsa.ngauction.websocket;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -11,44 +10,46 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
-	@Autowired
-	private SocketHandler handler;
-	
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(handler, "/").setAllowedOrigins("*")
-				// initial Request/Handshake interceptor
-				.addInterceptors(new HttpSessionHandshakeInterceptor() {
+    @Autowired
+    private SocketHandler handler;
 
-					@Override
-					public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
-							WebSocketHandler wsHandler, @Nullable Exception ex) {
-						super.afterHandshake(request, response, wsHandler, ex);
-					}
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/").setAllowedOrigins("*")
+                // initial Request/Handshake interceptor
+                .addInterceptors(new HttpSessionHandshakeInterceptor() {
 
-					@Override
-					public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
-							WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-						boolean b = super.beforeHandshake(request, response, wsHandler, attributes);
-						// && (request.getPrincipal()).isAuthenticated();
-						return b;
-					}
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                               WebSocketHandler wsHandler, @Nullable Exception ex) {
+                        super.afterHandshake(request, response, wsHandler, ex);
+                    }
 
-				});
-	}
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                                   WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        boolean b = super.beforeHandshake(request, response, wsHandler, attributes);
+                        // && (request.getPrincipal()).isAuthenticated();
+                        return b;
+                    }
 
-	// @Bean
-	// public ServletServerContainerFactoryBean createWebSocketContainer() {
-	// ServletServerContainerFactoryBean container = new
-	// ServletServerContainerFactoryBean();
-	// container.setMaxTextMessageBufferSize(8192);
-	// container.setMaxBinaryMessageBufferSize(8192);
-	// return container;
-	// }
+                });
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
+    }
 
 }
